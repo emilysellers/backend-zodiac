59035ddc3fce596379ccbb06db75eb0ff1894c94
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-01 14:27:41 -0700

    add /zodiac route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,20 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { zodiac } = require('../lib/zodiac-data');
+
+// describe('example test - you should probably update me', () => {
+//   it('home route should return hello world', async () => {
+//     const resp = await request(app).get('/');
+//     expect(resp.text).toEqual('hello world!');
+//   });
+// });
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiac', async () => {
+    const resp = await request(app).get('/zodiac');
+    const expected = zodiac.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(resp.body).toEqual(expected);
   });
 });
